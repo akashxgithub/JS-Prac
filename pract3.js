bad34256e46ce082445a8a4d44a0d00027710d86
Akash <147654193+akashxgithub@users.noreply.github.com>
2024-03-14 19:33:07 +0530

    Add files via upload

---
+++ pract3.js
@@ -0,0 +1,30 @@
+// data types in js
+
+// primitive
+// null - doesnot contain any value
+// Boolean - true or false 
+// undefined - variables are declared but values are not assigned
+// Number - integers and float 
+// String - combination of diff characters 
+// Symbol - unique identifier
+
+
+// nonprimitive 
+// objects - key-value pairs 
+// Array - collection of Elements, represented with []
+
+
+// ways to write CSS
+// external File - creating a separate css file and linking it to html file
+// using style tag in Header of html 
+// inline css - using style
+
+// inline                                  inline - block
+// - take width as much as necessary      - have specific width and height
+// -ignore vertical m and p               -respect vertical m and p
+// -do not respect vertical               - respect vertical align property
+//  align property 
+// -do not start on new line              -do not start on new line  
+
+
+
